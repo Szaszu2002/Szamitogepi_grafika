fb08d10d35943ae9995e9ce26ad1f1be3a20058d
Szaszu2002 <szaboalexandram18@gmail.com>
2023-05-09 14:24:16 +0200

    feladat 0.2

--- feladat/src/main.c
+++ feladat/src/main.c
@@ -1,7 +1,18 @@
+#include "app.h"
+
 #include <stdio.h>
 
 int main()
 {
+    App app;
+
+    init_app(&app, 800, 600);
+    while (app.is_running) {
+        handle_app_events(&app);
+        update_app(&app);
+        render_app(&app);
+    }
+    destroy_app(&app);
 
     return 0;
 }
